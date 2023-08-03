4eb86db2fc76244cfc13447b89dc4c9d33bd60f7
brunobucard <brunobucard@gmail.com>
2023-08-03 00:05:46 -0300

    first commit

---
+++ src/test/java/com/desafiomercadolivre/DesafioMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiomercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
